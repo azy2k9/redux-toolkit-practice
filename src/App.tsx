58ad7f1ec85c2b3003cd591f9ee225cf284ce155
Arslaan Qadus <azy2k9@gmail.com>
2022-05-22 13:55:14 +0100

    Basic redux slices and store setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { Button, Grid, Typography } from '@material-ui/core';
 import React from 'react';
+import { useAppDispatch, useAppSelector } from './app/hooks';
+import { incremented, amountAdded } from './features/counter/counter-slice';
 
-function App() {
-    return <div>hello</div>;
-}
+const App = () => {
+    const count = useAppSelector((state) => state.counter.value);
+    const dispatch = useAppDispatch();
+
+    function handleClick() {
+        dispatch(incremented());
+    }
+
+    function incrementBy5() {
+        dispatch(amountAdded(5));
+    }
+
+    return (
+        <Grid container direction='column'>
+            <Typography>The current count is {count}</Typography>
+            <Button onClick={handleClick}>Increment</Button>
+            <Button onClick={incrementBy5}>Increment by 5</Button>
+        </Grid>
+    );
+};
 
 export default App;
